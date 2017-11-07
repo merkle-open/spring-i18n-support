f0f0d1583b6f51a6cbde04461b34a70210f898a5
Lorenz Bösch <lorenz.boesch@namics.com>
2017-11-07 13:27:34 +0100

    add csrf support.

--- spring-i18n-support-web/src/main/resources/META-INF/terrific/assets/js/terrific-start.js
+++ spring-i18n-support-web/src/main/resources/META-INF/terrific/assets/js/terrific-start.js
@@ -24,6 +24,53 @@ if (!Array.prototype.filter) {
 	};
 }
 
+(function () {
+	window.T = window.T || {};
+	T.Utils = T.Utils || {};
+
+	var _checkDomain = function (url) {
+		if (url.indexOf('//') === 0) {
+			url = location.protocol + url;
+		}
+		return url.toLowerCase().replace(/([a-z])?:\/\//, '$1').split('/')[0];
+	};
+
+	T.Utils.isExternal = function (url) {
+		return ( ( url.indexOf(':') > -1 || url.indexOf('//') > -1 ) && _checkDomain(location.href) !== _checkDomain(url) );
+	};
+
+	T.Utils.getParam = function (name, url) {
+		if (!url) {
+			url = window.location.href;
+		}
+		name = name.replace(/[\[\]]/g, "\\$&");
+		var regex = new RegExp("[#?&]" + name + "(=([^&#]*)|&|#|$)"),
+			results = regex.exec(url);
+		if (!results) {
+			return null;
+		}
+		if (!results[2]) {
+			return '';
+		}
+		return decodeURIComponent(results[2].replace(/\+/g, " "));
+	};
+
+})();
+
+jQuery(document).ready(function ($) {
+
+	var token = T.Utils.getParam("csrf"),
+		header = T.Utils.getParam("csrf_header");
+
+	if (token && header) {
+		$(document).ajaxSend(function (e, xhr, settings) {
+			if (!T.Utils.isExternal(settings.url)) {
+				xhr.setRequestHeader(header, token);
+			}
+		});
+	}
+}(jQuery));
+
 /** Initializer */
 (function ($) {
 	$(document).ready(function () {
